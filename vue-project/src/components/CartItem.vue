18c86bf4aaf15bd1f4f2dbb33975bc99b1fb033f
AltinRuslan <altinbaev.ruslan.2003@gmail.com>
2023-12-25 21:38:52 +0300

    vue-sneakers

---
+++ vue-project/src/components/CartItem.vue
@@ -0,0 +1,29 @@
+<script setup>
+const emit = defineEmits(['onClickRemove'])
+
+
+  defineProps({
+    id: Number,
+    title: String,
+    imageUrl: String,
+    price: Number
+  })
+</script>
+
+<template>
+  <div class="flex border items-center border-slate-100 p-4 rounded-xl gap-4">
+    <img class="w-16 h-16" :src="imageUrl" :alt="title">
+    <div class="flex flex-col flex-1 ">
+      <p>{{ title}}</p>
+
+      <div class="flex justify-between mt-2">
+        <b class="flex-1">{{  price }}</b>
+        <img @click="emit('onClickRemove')" src="/close.svg" class="opacity-40 hover:opacity-100 cursor-pointer transition"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/pages/Favorites.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref, onMounted} from "vue";
+import axios from 'axios';
+import CardList from "@/components/CardList.vue";
+
+const favorites = ref([])
+
+onMounted(async () => {
+  try {
+    const {data} = await axios.get(
+        'https://6e1dc1bb021dad8a.mokky.dev/favorites?_relations=items'
+    )
+
+    favorites.value = data.map((obj) => obj.item)
+  } catch (err) {
+    console.log(err)
+  }
+})
+</script>
+
+<template>
+  <h2 class="text-3xl font-bold mb-8">Мои закладки</h2>
+  <CardList :items="favorites" is-favorites/>
+</template>
+
+<style scoped>
+
+</style>
